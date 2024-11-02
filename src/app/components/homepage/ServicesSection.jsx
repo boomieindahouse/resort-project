27625e71d1c629632142463287a91a7ab54babcc
boomieindahouse <prawitsanat@hotmail.com>
2024-11-02 16:27:48 +0700

    edit facilities section

--- src/app/components/homepage/ServicesSection.jsx
+++ src/app/components/homepage/ServicesSection.jsx
@@ -1,22 +1,24 @@
-// components/ServicesSection.js
 export default function ServicesSection() {
     return (
-        <div className="bg-gray-100 p-8 flex justify-around text-center bg-gray-300">
-            <div>
-                <div className="text-3xl">🚗</div>
-                <p>Free Parking</p>
-            </div>
-            <div>
-                <div className="text-3xl">⏰</div>
-                <p>Open 24 hours</p>
-            </div>
-            <div>
-                <div className="text-3xl">📞</div>
-                <p>Call to Order</p>
-            </div>
-            <div>
-                <div className="text-3xl">🔒</div>
-                <p>CCTV Security</p>
+        <div className="bg-gray-100 p-8 text-center bg-gray-400">
+            <h1 className="text-3xl font-bold mb-6">Services & Facilities</h1>
+            <div className="flex justify-around">
+                <div>
+                    <div className="text-3xl">🚗</div>
+                    <p>Free Parking</p>
+                </div>
+                <div>
+                    <div className="text-3xl">⏰</div>
+                    <p>Open 24 hours</p>
+                </div>
+                <div>
+                    <div className="text-3xl">📞</div>
+                    <p>Call to Order</p>
+                </div>
+                <div>
+                    <div className="text-3xl">🔒</div>
+                    <p>CCTV Security</p>
+                </div>
             </div>
         </div>
     );
