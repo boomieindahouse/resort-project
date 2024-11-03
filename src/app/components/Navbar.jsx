c1c82a6ca291a14cc4beaa6606e1f9e161e4aa77
boomieindahouse <prawitsanat@hotmail.com>
2024-11-03 16:31:18 +0700

    edit aboutsection and navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,17 +6,17 @@ export default function Navbar() {
         <nav className="bg-gray-700 text-white p-5 flex justify-between items-center fixed top-0 left-0 w-full z-50">
             {/* left side */}
             <Link href='/' className="text-lg font-bold px-10">Tonnam Resort</Link>
-            
+
             {/* mid */}
             <div className="flex-grow text-center">
-            <Link href="#about" className='px-5'>
+                <Link href="#about" className='px-5'>
                     About
-                </Link>
-                <Link href="#rooms" className='px-5'>
-                    Rooms
                 </Link>
                 <Link href="#facility" className='px-5'>
                     Facilities
+                </Link>
+                <Link href="#rooms" className='px-5'>
+                    Rooms
                 </Link>
             </div>
 
--- src/app/components/homepage/AboutSection.jsx
+++ src/app/components/homepage/AboutSection.jsx
@@ -1,20 +1,23 @@
 // components/AboutSection.js
 export default function AboutSection() {
     return (
-        <div className="flex justify-around h-[700px] bg-gray-100 flex justify-center items-center">
-            <img
-                src="https://image-placeholder.com/images/actual-size/600x400.png"
-                alt="Resort placeholder"
-                className="object-cover"
-            />
-            <div className="w-1/3 text-left">
-                <h3 className="text-xl font-semibold">Welcome To My Resort</h3>
-                <p className="mt-2 text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quae rem nemo iste ut totam harum repellat esse quod voluptatum.
-                </p>
-                <p className="mt-2 text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur. Metus elit leo pretium amet...
-                </p>
+        <div className="text-center py-28 bg-gray-100">
+            <h1 className="text-4xl font-bold mb-4">About Resort</h1>
+            <div className="flex justify-around h-[600px] items-center">
+                <img
+                    src="https://image-placeholder.com/images/actual-size/600x400.png"
+                    alt="Resort placeholder"
+                    className="object-cover"
+                />
+                <div className="w-1/3 text-left">
+                    <h3 className="text-xl font-semibold">Welcome To My Resort</h3>
+                    <p className="mt-2 text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quae rem nemo iste ut totam harum repellat esse quod voluptatum.
+                    </p>
+                    <p className="mt-2 text-gray-600">
+                        Lorem ipsum dolor sit amet consectetur. Metus elit leo pretium amet...
+                    </p>
+                </div>
             </div>
         </div>
     );
